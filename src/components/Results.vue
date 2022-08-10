f479c11c6e0465fed96ec2994c8aa40c2df34dfd
Hosea Patricia Corpuz <Hosea.Corpuz@EntainGroup.com>
2022-08-10 15:35:31 +0800

    first commit

---
+++ src/components/Results.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+    <p>Reaction time: {{ showScoreToComponent }} ms</p>
+    <h1 :class="txtColor"> {{ rank }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['showScoreToComponent' , 'theme'],
+    data() {
+        return {
+            rank: null,
+            txtColor: null
+        }
+    },
+    mounted() {
+        if (this.showScoreToComponent < 250 ) {
+            this.rank = "Awsome! You had a Ninja Fingers"
+            this.txtColor = 'txt-green'
+            console.log('green')
+        }
+        else if (this.showScoreToComponent < 400) {
+            this.rank = "Rapid Reflexes"
+            this.txtColor = 'txt-yellow'
+            console.log('yellow')
+        }
+        else {
+            this.rank = "Too Slow! Try Again.."
+            this.txtColor = 'txt-red'
+            console.log('red')
+        }
+    }
+}
+</script>
+
+<style>
+.txt-green{
+    color: forestgreen;
+}
+.txt-yellow{
+    color:goldenrod;
+}
+.txt-red{
+    color:crimson;
+}
+
+</style>
