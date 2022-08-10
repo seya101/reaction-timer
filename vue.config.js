df94ded058749b5dbb7d34c2c59f68141ab98b6d
Hosea Patricia Corpuz <Hosea.Corpuz@EntainGroup.com>
2022-08-10 16:04:42 +0800

    update all file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // vue.config.js file to be placed in the root of your repository
 
-module.exports = {
-    publicPath: '/reaction-timer/'
-  }
+    module.exports = {
+        publicPath: process.env.NODE_ENV === "production" ? "/reaction-timer/" : "/",
+      };
